a0da553c7494187305ed130ee6e86517d45b2cc7
tingpeng <tingpengseet@gmail.com>
2021-04-25 21:53:03 +0800

    updated founditems to lostitems matching function to search for categories and use object as query filter

--- BE/config/dbController.js
+++ BE/config/dbController.js
@@ -6,11 +6,12 @@ exports.findFoundItemsbyKeyword = function (req, res) {
     if (err) { res.send(err) }
     console.log('lostitem returned: ' + lostitem)
   }).then(lostitems => {
-    lostitems.keyword.forEach(word => FoundItem.find({ keyword: { $in: word } }, function (_err, docs) {
-      console.log('word: ' + word)
+    const testword = { category: { $in: lostitems.category } }
+    FoundItem.find(testword, function (_err, docs) {
       console.log('word type: ' + typeof word)
+      console.log('testword type: ' + typeof testword)
       console.log('document returned: ' + docs)
-    }))
+    })
   }).then(item => res.status(200).json({ message: 'ok' })).catch(_err => res.status(400).json({ error: _err.toString() }))
 }
 // foreach string in keywords, run a find and return the matched documents
